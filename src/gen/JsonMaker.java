6e6cbf0d1c33dd2b6fbe4e3bec6bcc9197f4ea75
hamidrezakamkari <hamidrezakamkari@gmail.com>
2019-05-03 21:00:39 +0430

    update JsonMaker

--- src/gen/JsonMaker.java
+++ src/gen/JsonMaker.java
@@ -1,10 +1,6 @@
 package gen;
 
-/*
- *  this class goes to ./ManualFeatureInputLogs and runs all of the files
- *  in it on Manual feature adder to make json files from it
- */
-
+import javax.print.attribute.standard.NumberUp;
 import java.io.File;
 import java.util.Iterator;
 
@@ -13,24 +9,29 @@ class JsonMaker {
     private static void deleteAll(String dir) {
         File myDir = new File(dir);
         File[] files = myDir.listFiles();
-        if (files != null) {
+        try {
             for (File file : files) {
                 if (!file.delete()) {
                     System.out.println("Failed To delete!");
                 }
             }
+        } catch (NullPointerException ignore) {
+            System.out.print("NULL POINTER EXCEPTION: ");
+            System.out.println(dir);
         }
     }
+
     public static void main(String[] args) {
-        deleteAll("./gamaData/Collectibles/");
+        deleteAll("./gameData/Usables/");
         deleteAll("./gameData/Heros/");
         deleteAll("./gameData/Minions/");
         deleteAll("./gameData/SpellCards/");
+        deleteAll("./gameData/Collectibles");
         File dir = new File("./gameData/ManualFeatureInputLogs/");
         File[] directoryListing = dir.listFiles();
         if (directoryListing != null) {
             for (File child : directoryListing) {
-                ManualFeatureAdder.main(new String[] {"java", "ManualFeatureAdder", child.toString()});
+                ManualFeatureAdder.main(new String[]{"java", "ManualFeatureAdder", child.toString()});
             }
         }
     }
