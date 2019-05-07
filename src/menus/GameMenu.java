3b37c740e9513e7fd8d9343883aa33d0c189c682
smsk <mohammadsadeghkeshavarzi@yahoo.com>
2019-05-07 19:16:45 +0430

    Added wins

--- src/menus/GameMenu.java
+++ src/menus/GameMenu.java
@@ -204,6 +204,7 @@ public class GameMenu extends InGameMenu {
             case WIN_FIRST_PLAYER:
                 account.payMoney(game.getPrize());
                 account.addMatch(new Match((hasAI ? null : secondAccount), Result.WIN, LocalDateTime.now()));
+                account.addWin();
                 if (!hasAI) {
                     secondAccount.addMatch(new Match(account, Result.WIN, LocalDateTime.now()));
                 }
@@ -214,6 +215,7 @@ public class GameMenu extends InGameMenu {
             case WIN_SECOND_PLAYER:
                 account.addMatch(new Match((hasAI ? null : secondAccount), Result.WIN, LocalDateTime.now()));
                 if (!(secondAccount == null)) {
+                    secondAccount.addWin();
                     secondAccount.addMatch(new Match(account, Result.WIN, LocalDateTime.now()));
                     secondAccount.payMoney(game.getPrize());
                     view.showWinner(secondAccount, game.getPrize());
