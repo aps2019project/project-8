e3e882005d4cbb9d966af30f638849ac1d9d8507
penhauer <ghasemiamirmohammad@yahoo.com>
2019-07-04 18:15:51 +0430

    Create client branch

--- server/src/model/ItemType.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public enum ItemType {
-    CAST_A_SPELL,
-    ADD_A_SPECIAL_POWER,
-    ADD_MANA
-}
-
--- server/src/model/UnitType.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public enum UnitType {
-    MELEE,
-    RANGED,
-    HYBRID
-}
-
